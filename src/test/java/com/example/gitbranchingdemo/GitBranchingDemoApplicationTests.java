c518d7dc251143d5a807513ba405efcaa950eef3
misc0001 <misc0001@stud.kea.dk>
2024-02-19 09:21:05 +0100

    Demo

---
+++ src/test/java/com/example/gitbranchingdemo/GitBranchingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitbranchingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitBranchingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
